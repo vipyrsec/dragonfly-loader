797ee0396f3f11e7893888c1fb76621e49a1f3ee
Robin5605 <robinjefferson123@gmail.com>
2023-07-22 21:37:47 -0500

    Add proper tests

--- src/cronjob/cronjob.py
+++ src/cronjob/cronjob.py
@@ -1,57 +1,33 @@
 from letsbuilda.pypi import PyPIServices
 from requests import Session
-from os import getenv
-from dotenv import load_dotenv
 
-load_dotenv()
+from cronjob.settings import Settings, get_settings
 
-BASE_URL = getenv("DRAGONFLY_API_URL", "https://dragonfly.vipyrsec.com")
-AUTH0_DOMAIN = getenv("AUTH0_DOMAIN", "vipyrsec.us.auth0.com")
-
-CLIENT_ID = getenv("CLIENT_ID")
-CLIENT_SECRET = getenv("CLIENT_SECRET")
-USERNAME = getenv("USERNAME")
-PASSWORD = getenv("PASSWORD")
-AUDIENCE = getenv("AUDIENCE", "https://dragonfly.vipyrsec.com")
-
-if not CLIENT_ID:
-    raise Exception("`CLIENT_ID` is a required environment variable!")
-
-if not CLIENT_SECRET:
-    raise Exception("`CLIENT_SECRET` is a required environment variable!")
-
-if not USERNAME:
-    raise Exception("`USERNAME` is a required environment variable!")
-
-if not PASSWORD:
-    raise Exception("`PASSWORD` is a required environment variable!")
-
-
-def get_access_token(*, http_session: Session) -> str:
+def get_access_token(*, http_session: Session, settings: Settings) -> str:
     payload = dict(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        username=USERNAME,
-        password=PASSWORD,
-        audience=AUDIENCE,
+        client_id=settings.client_id,
+        client_secret=settings.client_secret,
+        username=settings.username,
+        password=settings.password,
+        audience=settings.audience,
         grant_type="password",
     )
 
-    with http_session.post(f"https://{AUTH0_DOMAIN}/oauth/token", json=payload) as res:
-        json = res.json()
-        return json["access_token"]
-
+    res = http_session.post(f"https://{settings.auth0_domain}/oauth/token", json=payload)
+    json = res.json()
+    return json["access_token"]
 
 def main() -> None:
     http_session = Session()
     pypi_client = PyPIServices(http_session)
+    settings = get_settings()
 
-    access_token = get_access_token(http_session=http_session)
+    access_token = get_access_token(http_session=http_session, settings=settings)
 
     packages = pypi_client.get_rss_feed(PyPIServices.PACKAGE_UPDATES_FEED_URL)
     payload = [dict(name=package.title, version=package.version) for package in packages]
     headers = {"Authorization": "Bearer " + access_token}
 
-    http_session.post(f"{BASE_URL}/batch/package", json=payload, headers=headers)
+    http_session.post(f"{settings.base_url}/batch/package", json=payload, headers=headers)
 
     http_session.close()
--- tests/test_smoke.py
+++
@@ -1,8 +0,0 @@
-"""Stub test file"""
-
-from __future__ import annotations
-
-
-def test_smoke() -> None:
-    """Smoke test for CI"""
-    assert True is True
