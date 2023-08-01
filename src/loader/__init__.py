2aab44ff91d612b3313d58e732eb576267a492e3
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-07-31 19:49:46 -0500

    Configure Ruff to select ALL rules (#9)
    Signed-off-by: GitHub <noreply@github.com>

--- src/loader/__init__.py
+++ src/loader/__init__.py
@@ -1,1 +1,1 @@
-"""Loads all the packages from the PyPI RSS feed into the Dragonfly API"""
+"""Loads all the packages from the PyPI RSS feed into the Dragonfly API."""
--- src/loader/settings.py
+++ src/loader/settings.py
@@ -1,3 +1,5 @@
+"""Settings for the Dragonfly Loader."""
+
 from dataclasses import dataclass
 from os import getenv
 
@@ -6,6 +8,8 @@ from dotenv import load_dotenv
 
 @dataclass
 class Settings:
+    """Settings for the Dragonfly Loader."""
+
     base_url: str
     auth0_domain: str
 
@@ -19,35 +23,44 @@ class Settings:
 
 
 def get_settings() -> Settings:
+    """Get the settings for the Dragonfly Loader."""
     load_dotenv()
 
-    BASE_URL = getenv("DRAGONFLY_API_URL", "https://dragonfly.vipyrsec.com")
-    AUTH0_DOMAIN = getenv("AUTH0_DOMAIN", "vipyrsec.us.auth0.com")
+    base_url = getenv("DRAGONFLY_API_URL", "https://dragonfly.vipyrsec.com")
+    auth0_domain = getenv("AUTH0_DOMAIN", "vipyrsec.us.auth0.com")
 
-    CLIENT_ID = getenv("CLIENT_ID")
-    CLIENT_SECRET = getenv("CLIENT_SECRET")
-    USERNAME = getenv("USERNAME")
-    PASSWORD = getenv("PASSWORD")
-    AUDIENCE = getenv("AUDIENCE", "https://dragonfly.vipyrsec.com")
+    client_id = getenv("CLIENT_ID")
+    client_secret = getenv("CLIENT_SECRET")
+    username = getenv("USERNAME")
+    password = getenv("PASSWORD")
+    audience = getenv("AUDIENCE", "https://dragonfly.vipyrsec.com")
 
-    if not CLIENT_ID:
-        raise Exception("`CLIENT_ID` is a required environment variable!")
+    if not client_id:
+        msg = "`CLIENT_ID` is a required environment variable!"
+        raise Exception(msg)  # noqa: TRY002 - TODO @Robin5605: Use a custom exception
+        # https://github.com/vipyrsec/dragonfly-loader/issues/10
 
-    if not CLIENT_SECRET:
-        raise Exception("`CLIENT_SECRET` is a required environment variable!")
+    if not client_secret:
+        msg = "`CLIENT_SECRET` is a required environment variable!"
+        raise Exception(msg)  # noqa: TRY002 - TODO @Robin5605: Use a custom exception
+        # https://github.com/vipyrsec/dragonfly-loader/issues/10
 
-    if not USERNAME:
-        raise Exception("`USERNAME` is a required environment variable!")
+    if not username:
+        msg = "`USERNAME` is a required environment variable!"
+        raise Exception(msg)  # noqa: TRY002 - TODO @Robin5605: Use a custom exception
+        # https://github.com/vipyrsec/dragonfly-loader/issues/10
 
-    if not PASSWORD:
-        raise Exception("`PASSWORD` is a required environment variable!")
+    if not password:
+        msg = "`PASSWORD` is a required environment variable!"
+        raise Exception(msg)  # noqa: TRY002 - TODO @Robin5605: Use a custom exception
+        # https://github.com/vipyrsec/dragonfly-loader/issues/10
 
     return Settings(
-        base_url=BASE_URL,
-        auth0_domain=AUTH0_DOMAIN,
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        username=USERNAME,
-        password=PASSWORD,
-        audience=AUDIENCE,
+        base_url=base_url,
+        auth0_domain=auth0_domain,
+        client_id=client_id,
+        client_secret=client_secret,
+        username=username,
+        password=password,
+        audience=audience,
     )
