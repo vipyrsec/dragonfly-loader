0ea9377bbb23ac787cfb27ce4f78173026fb3ea1
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-10-26 02:35:17 +0000

    Use .env.sample file to avoid setting invalid defaults
    Signed-off-by: GitHub <noreply@github.com>

--- src/loader/constants.py
+++ src/loader/constants.py
@@ -4,6 +4,7 @@ Loads configuration from environment variables and `.env` files.
 By default, the values defined in the classes are used, these can be overridden by an env var with the same name.
 
 An `.env` file is used to populate env vars, if present.
+An `.env.sample` in the repo is used to apply generic values for development and CI.
 """
 
 from pydantic import SecretStr
@@ -14,22 +15,25 @@ class EnvConfig(BaseSettings):
     """Our default configuration for models that should load from .env files."""
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=(
+            ".env.sample",  # For documentation purposes, applies generic values for development and CI
+            ".env",
+        ),
         env_file_encoding="utf-8",
         env_nested_delimiter="__",
         extra="ignore",
     )
 
 
-class _Dragonfly(EnvConfig, env_prefix="dragonfly"):
+class _Dragonfly(EnvConfig, env_prefix="dragonfly_loader_"):
     """Configuration for the Dragonfly API."""
 
     base_url: str = "https://dragonfly.vipyrsec.com"
     auth0_domain: str = "vipyrsec.us.auth0.com"
-    client_id: SecretStr = ""
-    client_secret: SecretStr = ""
-    username: SecretStr = ""
-    password: SecretStr = ""
+    client_id: SecretStr
+    client_secret: SecretStr
+    username: SecretStr
+    password: SecretStr
     audience: str = "https://dragonfly.vipyrsec.com"
 
 
