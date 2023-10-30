0f4e7fe4c6279536aec82676838044d261d71671
Robin <74519799+Robin5605@users.noreply.github.com>
2023-10-29 22:06:04 -0500

    Refactor code to be more testable (#16)
    * Refactor code to be more testable
    
    * Lint

--- src/loader/__main__.py
+++ src/loader/__main__.py
@@ -1,6 +1,12 @@
 """Entry point for the loader."""
 
+from httpx import Client
+from letsbuilda.pypi import PyPIServices
+
 from .loader import main
 
 if __name__ == "__main__":
-    main()
+    http_client = Client()
+    pypi_client = PyPIServices(http_client=http_client)
+
+    main(http_client=http_client, pypi_client=pypi_client)
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,10 +1,11 @@
 """Stub test file."""
 
 from dataclasses import dataclass
-from unittest.mock import DEFAULT, MagicMock, Mock, patch
+from unittest.mock import Mock
 
 import pytest
 from loader import loader
+from loader.constants import Settings
 
 
 @dataclass
@@ -15,8 +16,8 @@ class MockPackage:
     version: str
 
 
-environment_variables = {
-    "DRAGONFLY_API_URL": "https://example.com",
+ENV = {
+    "BASE_URL": "https://example.com",
     "AUTH0_DOMAIN": "example-auth.com",
     "USERNAME": "test username",
     "PASSWORD": "test password",
@@ -26,67 +27,95 @@ environment_variables = {
 }
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def mock_env(monkeypatch: pytest.MonkeyPatch) -> None:
     """Mock the environment variables."""
-    for k, v in environment_variables.items():
-        monkeypatch.setenv(k, v)
+    for k, v in ENV.items():
+        monkeypatch.setattr(Settings, k.lower(), v)
 
 
-@pytest.fixture()
-def mock_rss_data() -> list[MockPackage]:
-    """Mock the RSS data."""
-    return [
+def test_build_authorization_header() -> None:
+    """Test that building authorization header works properly."""
+    expected = {"Authorization": "Bearer token"}
+    actual = loader.build_authorization_header("token")
+
+    assert expected == actual
+
+
+def test_get_access_token() -> None:
+    """Test that getting access token works properly."""
+    json_return_mock = {"access_token": "abc"}
+    attrs = {"post.return_value.json.return_value": json_return_mock}
+    mock_client = Mock(**attrs)
+    access_token = loader.get_access_token(http_client=mock_client)
+
+    expected_url = f"https://{ENV['AUTH0_DOMAIN']}/oauth/token"
+    expected_payload = {
+        "client_id": ENV["CLIENT_ID"],
+        "client_secret": ENV["CLIENT_SECRET"],
+        "username": ENV["USERNAME"],
+        "password": ENV["PASSWORD"],
+        "audience": ENV["AUDIENCE"],
+        "grant_type": "password",
+    }
+
+    mock_client.post.assert_called_with(expected_url, json=expected_payload)
+    assert access_token == "abc"
+
+
+def test_fetch_packages() -> None:
+    """Test that getting packages from the RSS feed works properly."""
+    mock_packages = [
         MockPackage(title="a", version="1.0.0"),
         MockPackage(title="b", version="1.0.1"),
-        MockPackage(title="c", version="1.0.2"),
+    ]
+    attrs = {"get_rss_feed.return_value": mock_packages}
+    mock_pypi_client = Mock(**attrs)
+
+    actual = loader.fetch_packages(pypi_client=mock_pypi_client)
+    expected = [("a", "1.0.0"), ("b", "1.0.1")]
+
+    assert actual == expected
+
+
+def test_load_packages() -> None:
+    """Test that loading packages into the Dragonfly API works properly."""
+    mock_client = Mock()
+    mock_client.post = Mock()
+    mock_packages = [
+        ("a", "1.0.0"),
+        ("b", "1.0.1"),
     ]
 
+    loader.load_packages(mock_packages, http_client=mock_client, access_token="test access token")
 
-def mock_http_session_post_side_effect(*args: list, **_kwargs: dict) -> Mock:
-    """Mock the HTTP auth call."""
-    if args[0] == "https://" + environment_variables["AUTH0_DOMAIN"] + "/oauth/token":
-        mock = Mock()
-        mock.json = Mock()
-        mock.json.return_value = {
-            "access_token": "test-access-token",
-            "token_type": "Bearer",
-            "expires_in": 86400,
-        }
-
-        return mock
-
-    return DEFAULT
+    expected_payload = [{"name": name, "version": version} for name, version in mock_packages]
+    expected_headers = {"Authorization": "Bearer test access token"}
+    mock_client.post.assert_called_once_with(
+        f"{ENV['BASE_URL']}/batch/package",
+        json=expected_payload,
+        headers=expected_headers,
+    )
 
 
-def test_loader(mock_env, mock_rss_data: list[MockPackage]) -> None:  # noqa: ANN001 - unclear
-    """Test the loader."""
-    mock_http_client = MagicMock()
-    mock_http_client.post = MagicMock(side_effect=mock_http_session_post_side_effect)
+def test_main(monkeypatch: pytest.MonkeyPatch) -> None:
+    """Test that everything works together."""
+    mock_packages = [
+        ("a", "1.0.0"),
+        ("b", "1.0.1"),
+    ]
 
-    mock_pypi_client = MagicMock()
-    mock_pypi_client.get_rss_feed = MagicMock(return_value=mock_rss_data)
+    mock_http_client = Mock()
+    mock_pypi_client = Mock()
 
-    with patch.multiple(
-        "loader.loader",
-        Client=Mock(return_value=mock_http_client),
-        PyPIServices=Mock(return_value=mock_pypi_client),
-    ):
-        loader.main()
-        mock_http_client.post.assert_any_call(
-            f"https://{environment_variables['AUTH0_DOMAIN']}/oauth/token",
-            json={
-                "client_id": environment_variables["CLIENT_ID"],
-                "client_secret": environment_variables["CLIENT_SECRET"],
-                "username": environment_variables["USERNAME"],
-                "password": environment_variables["PASSWORD"],
-                "audience": environment_variables["AUDIENCE"],
-                "grant_type": "password",
-            },
-        )
+    fetch_packages_mock = Mock(return_value=mock_packages)
+    load_packages_mock = Mock()
+    get_access_token_mock = Mock(return_value="test-access-token")
+    monkeypatch.setattr("loader.loader.fetch_packages", fetch_packages_mock)
+    monkeypatch.setattr("loader.loader.load_packages", load_packages_mock)
+    monkeypatch.setattr("loader.loader.get_access_token", get_access_token_mock)
 
-        mock_http_client.post.assert_any_call(
-            f"{environment_variables['DRAGONFLY_API_URL']}/batch/package",
-            json=[{"name": p.title, "version": p.version} for p in mock_rss_data],
-            headers={"Authorization": "Bearer test-access-token"},
-        )
+    loader.main(http_client=mock_http_client, pypi_client=mock_pypi_client)
+    get_access_token_mock.assert_any_call(http_client=mock_http_client)
+    fetch_packages_mock.assert_any_call(pypi_client=mock_pypi_client)
+    load_packages_mock.assert_any_call(mock_packages, http_client=mock_http_client, access_token="test-access-token")
