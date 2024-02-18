9aec806f3e1cd8d8c5eb71c8eadb28abda354376
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-02-18 03:36:36 +0000

    Bump all dependencies
    Signed-off-by: GitHub <noreply@github.com>

---
+++ make.ps1
@@ -0,0 +1,121 @@
+<#
+.SYNOPSIS
+Makefile
+
+.DESCRIPTION
+USAGE
+    .\make.ps1 <command>
+
+COMMANDS
+    init              install Python build tools
+    install-dev       install local package in editable mode
+    update-deps       update the dependencies
+    upgrade-deps      upgrade the dependencies
+    lint              run `pre-commit` and `ruff` and `mypy`
+    test              run `pytest`
+    build-dist        run `python -m build`
+    clean             delete generated content
+    help, -?          show this help message
+#>
+param(
+    [Parameter(Position = 0)]
+    [ValidateSet("init", "install-dev", "update-deps", "upgrade-deps", "lint", "test", "build-dist", "clean", "help")]
+    [string]$Command
+)
+
+function Invoke-Help
+{
+    Get-Help $PSCommandPath
+}
+
+function Invoke-Init
+{
+    python -m pip install --upgrade pip wheel setuptools build
+}
+
+function Invoke-Install-Dev
+{
+    python -m pip install --upgrade --editable .
+}
+
+function Invoke-Update-Deps
+{
+    python -m pip install --upgrade pip-tools
+    pip-compile requirements/requirements.in
+}
+
+function Invoke-Upgrade-Deps
+{
+    python -m pip install --upgrade pip-tools pre-commit
+    pre-commit autoupdate
+    pip-compile --upgrade requirements/requirements.in
+}
+
+function Invoke-Lint
+{
+    pre-commit run --all-files
+    python -m ruff --fix .
+    python -m ruff format .
+    python -m mypy --strict src/
+}
+
+function Invoke-Test
+{
+    python -m pytest
+}
+
+function Invoke-Build-Dist
+{
+    python -m pip install --upgrade build
+    python -m build
+}
+
+function Invoke-Clean
+{
+    $folders = @("build", "dist")
+    foreach ($folder in $folders)
+    {
+        if (Test-Path $folder)
+        {
+
+            Write-Verbose "Deleting $folder"
+            Remove-Item $folder -Recurse -Force
+        }
+    }
+}
+
+switch ($Command)
+{
+    "init"    {
+        Invoke-Init
+    }
+    "install-dev" {
+        Invoke-Install-Dev
+    }
+    "lint"  {
+        Invoke-Lint
+    }
+    "update-deps"  {
+        Invoke-Update-Deps
+    }
+    "upgrade-deps"  {
+        Invoke-Upgrade-Deps
+    }
+    "test"    {
+        Invoke-Test
+    }
+    "build-dist"    {
+        Invoke-Build-Dist
+    }
+    "clean"    {
+        Invoke-Clean
+    }
+    "help"  {
+        Invoke-Help
+    }
+    default
+    {
+        Invoke-Init
+        Invoke-Install-Dev
+    }
+}
